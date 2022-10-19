65e34d5b9ac3b2127bfcd545f1d5a0e212b687a8
bongobirch <84354402+bongobirch@users.noreply.github.com>
2022-10-19 00:34:57 -0500

    Standard Atmosphere Models
    atm.m is a function that returns a structure, this structure inclues temperature, pressure, and rho at the given altitude. This is in imperial units!

---
+++ PreetsCode.m
@@ -0,0 +1,118 @@
+%% Aero 4710 Launch Vehicle Design
+% Group Homework 1: Standard Atmosphere Model
+% Author: Jacob Birch, Zach Eljich, Henry Humphrey, Preet Shah
+% Date: 08/29/2022
+%References: Anderson, J.D, Introduction to Flight, 2022.
+%Hawley, Jim, Formulae and code for the U.S. Standard atmosphere, 1976.
+clc; clear all; close all;
+
+%% Constants
+Ti = [288.16,216.66,216.66,282.66,282.66,165.66,165.66,225.66]; %base temperatures [K]
+g = 9.81; %acceleration due to gravity [m/s^2]
+R = 287; %gas constant [J/(Kg*K)]
+gama = 1.4; %specific heat ratios 
+beta = 1.458 * 10^-6; %coefficient of air [kg/m.sec.sqrt(K)]
+S = 110.4; %sutherland's constant for air [K]
+
+for z = 1:105001
+if z <= 11000
+    j = 1; %index
+    lamda = -6.5/1000; %gradient
+    Tn = Ti(j);
+    T(z) = Tn + ((z-0)*lamda); %Temperature
+    p(z) = 101.325 * exp(-(g/(R*Tn))*(z-0)); %pressure
+    rho(z) = p(z)/(R*T(z)); %density
+elseif z <= 25000
+    j = 2;
+    lamda = 0;
+    Tn = Ti(j);
+    T(z) = Tn + ((z-11000)*lamda);
+    p(z) = p(11000) * exp(-(g/(R*Tn))*(z-11000));
+    rho(z) = p(z)/(R*T(z));
+elseif z <= 47000
+    j = 3;
+    lamda = 3/1000;
+    Tn = Ti(j);
+    T(z) = Tn + ((z-25000)*lamda);
+    p(z) = p(25000) * exp(-(g/(R*Tn))*(z-25000));
+    rho(z) = p(z)/(R*T(z));
+   
+elseif z <= 53000
+    j = 4;
+    lamda =0;
+    Tn = Ti(j);
+    T(z) = Tn + ((z-47000)*lamda);
+    p(z) = p(47000) * exp(-(g/(R*Tn))*(z-47000));
+    rho(z) = p(z)/(R*T(z));
+elseif z <= 79000
+    j =5;
+    lamda = -4.5/1000;
+    Tn = Ti(j);
+    T(z) = Tn + ((z-53000)*lamda);
+    p(z) = p(53000) * exp(-(g/(R*Tn))*(z-53000));
+    rho(z) = p(z)/(R*T(z));
+elseif z <= 90000
+    j =6;
+    lamda = 0;
+    Tn =Ti(j);
+    T(z) = Tn + ((z-79000)*lamda);
+    p(z) = p(79000) * exp(-(g/(R*Tn))*(z-79000));
+    rho(z) = p(z)/(R*T(z));
+else
+    j =7;
+    lamda = 4/1000;
+    Tn = Ti(j);
+    T(z) = Tn + ((z-90000)*lamda);
+    p(z) = p(90000) * exp(-(g/(R*Tn))*(z-90000));
+    rho(z) = p(z)/(R*T(z));
+
+end 
+end
+aPREET = sqrt(gama*R.*T); %speed of sound
+mu = (beta.*(T.^(3/2)))./(T+S); %viscosity
+h = [0:105000];
+
+rho = rho*1000;
+%% Plotting
+figure(1)
+hold on
+box on
+grid on
+plot(T,h,'LineWidth',2)
+title('Temperature vs Altitude ')
+xlabel('Temperature [K]')
+ylabel('Altitude [m]')
+figure(2)
+hold on
+box on
+grid on
+plot(p,h,'LineWidth',2)
+title('Pressure vs Altitude')
+xlabel('Pressure [Pa]')
+ylabel('Altitude [m]')
+figure (3)
+hold on
+box on
+grid on
+plot(rho,h,'LineWidth',2)
+title('Density vs Altitude')
+xlabel('Density [kg/m^3]')
+ylabel('Altitude [m]')
+figure (4)
+hold on
+box on
+grid on
+plot(aPREET,h,'LineWidth',2)
+title('Speed of Sound vs Altitude')
+xlabel('Speed of Sound [m/s]')
+ylabel('Altitude [m]')
+figure (5)
+hold on
+box on
+grid on
+plot(mu,h,'LineWidth',2)
+title('Viscosity vs Altitude')
+xlabel('Viscosity [N*s/m^2]')
+ylabel('Altitude[m]')
+
+    
---
+++ atm.m
@@ -0,0 +1,103 @@
+
+function prop = atm(alt)
+
+
+alt = alt/3.28084; % m to ft
+T0 = 288;
+P0 = 101325;
+g0 = -9.81; 
+R = 287.05; 
+h0 = 0; 
+
+% Lapse rates for each layer:
+l1 = -0.0065;
+l3 = 0.001;
+l4 = 0.0028;
+% L5 and L2 are not needed
+l6 = -0.0028;
+l7 = -0.002;
+
+if alt >= 0 && alt <= 11000
+    T = T0 + l1 * (alt - h0);
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    rho = P/(R * T);
+elseif alt > 11000 && alt <= 20000
+    T = T0 + l1*(11000 - h0);
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(alt-11000));
+    rho = P/(R * T);
+elseif alt > 20000 && alt <= 32000
+    T = T0 + l1*(11000 - h0);
+    T1 = T; 
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(20000-11000));
+    T = T + l3*(alt - 20000);
+    P = P * (T/T1)^(g0/(l3 * R));
+    rho = P/(R * T);
+elseif alt > 32000 && alt <= 47000
+    T = T0 + l1*(11000 - h0);
+    T1 = T;
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(20000-11000));
+    T = T + l3*(32000 - 20000);
+    T2 = T;
+    P = P * (T/T1)^(g0/(l3 * R));
+    T = T + l4*(alt - 32000);
+    P = P * (T/T2)^(g0/(l4 * R));
+    rho = P/(R * T);
+elseif alt > 47000 && alt <= 51000
+    T = T0 + l1*(11000 - h0);
+    T1 = T;
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(20000-11000));
+    T = T + l3*(32000 - 20000);
+    P = P * (T/T1)^(g0/(l3 * R));
+    T2 = T;
+    T = T + l4*(47000 - 32000);
+    P = P * (T/T2)^(g0/(l4 * R));
+    P = P * exp(((g0/(R * T)))*(alt-47000));
+    rho = P/(R * T);
+elseif alt > 51000 && alt <= 71000
+    T = T0 + l1*(11000 - h0);
+    T1 = T;
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(20000-11000));
+    T = T + l3*(32000 - 20000);
+    P = P * (T/T1)^(g0/(l3 * R));
+    T2 = T;
+    T = T + l4*(47000 - 32000);
+    T3 = T;
+    P = P * (T/T2)^(g0/(l4 * R));
+    P = P * exp(((g0/(R * T)))*(51000-47000));
+    T = T + l6*(alt - 51000);
+    P = P * (T/T3)^(g0/(l6 * R));
+    rho = P/(R * T);
+elseif alt > 71000
+    T = T0 + l1*(11000 - h0);
+    T1 = T;
+    P = P0 * (T/T0)^(g0/(l1 * R));
+    P = P * exp(((g0/(R * T)))*(20000-11000));
+    T = T + l3*(32000 - 20000);
+    P = P * (T/T1)^(g0/(l3 * R));
+    T2 = T;
+    T = T + l4*(47000 - 32000);
+    T3 = T;
+    P = P * (T/T2)^(g0/(l4 * R));
+    P = P * exp(((g0/(R * T)))*(51000-47000));
+    T = T + l6*(71000 - 51000);
+    T4 = T;
+    P = P * (T/T3)^(g0/(l6 * R));
+    T = T + l7*(alt - 71000);
+    P = P * (T/T4)^(g0/(l7 * R));
+    rho = P/(R * T);
+end
+
+% Time to convert
+T = T*1.8;
+P = P/47.88; 
+rho = rho/515.379;
+
+prop.T = T;
+prop.P = P;
+prop.rho = rho;
+end
